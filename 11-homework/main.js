2b415d8d04353fe2b5148d8ec3bef8fc79db2114
varkhol.yuliia <juljailchenko@gmail.com>
2020-08-26 10:04:36 +0300

    Homework#11

---
+++ 11-homework/main.js
@@ -0,0 +1,28 @@
+const DELAY = 50;
+let btn = document.querySelector(".btn");
+let length = document.querySelector(".length-input");
+let result = document.querySelector(".result");
+
+const getRandomChinese = (length) => {
+    return new Promise((resolve) => {
+
+        setTimeout(() => {
+            let characters = [];
+            let sign = parseInt(Date.now().toString().slice(-length));
+
+            for (let i = 0; i < length; i++) {
+                sign += i;
+                characters.push(sign);
+            }
+
+            resolve(String.fromCodePoint(...characters))
+        }, length * DELAY);
+    });
+};
+
+btn.addEventListener("click", () => {
+    getRandomChinese(length.value)
+        .then(symbols => {
+            result.innerText = `Chinese symbols:${symbols}`;
+        })
+});
